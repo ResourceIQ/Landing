2cfcd9909fe15980a88d32d273b17f6da76e8e4b
Diluka Lahiru <dilukalahiruofficial@gmail.com>
2025-11-24 10:38:44 +0530

    add about page

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function AboutPage() {
+  return (
+    <main className="min-h-screen px-6 py-20">
+      <h1 className="text-4xl font-bold mb-6">About Us</h1>
+      <p className="text-lg text-gray-600">
+        Welcome to our About page! You can update this content however you like.
+      </p>
+    </main>
+  );
+}
--- components/header.tsx
+++ components/header.tsx
@@ -6,11 +6,12 @@ import { Button } from '@/components/ui/button'
 import React from 'react'
 import { ModeToggle } from './ui/mode-toggle'
 
+
 const menuItems = [
     { name: 'Features', href: '#link' },
     { name: 'Solution', href: '#link' },
     { name: 'Pricing', href: '#link' },
-    { name: 'About', href: '#link' },
+    { name: 'About', href:'/about' },
 ]
 
 export const HeroHeader = () => {
