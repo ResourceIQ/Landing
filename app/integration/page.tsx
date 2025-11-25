c0139eea5ef4b1babfe0761cf06c14f035e85b8c
Diluka Lahiru <106172155+dilukalahiru@users.noreply.github.com>
2025-11-25 12:23:01 +0530

    add integrations

---
+++ app/integration/page.tsx
@@ -0,0 +1,13 @@
+import IntegrationsSection from '@/components/integrations-6';
+import { HeroHeader } from '../../components/header'
+import Footer from "@/components/footer";
+
+export default function PricingPage() {
+  return (
+    <>
+      <HeroHeader />
+      <IntegrationsSection />
+      <Footer />
+    </>
+  );
+}
