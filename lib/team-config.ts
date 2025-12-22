d2af4722fd051c8ca169ad9bdf3afe92bdc953ef
Diluka Lahiru <106172155+dilukalahiru@users.noreply.github.com>
2025-12-22 20:06:14 +0530

    Preregistaion all are complete and done

---
+++ lib/team-config.ts
@@ -0,0 +1,10 @@
+export const RESOURCE_IQ_TEAM = [
+  { name: "Diluka Lahiru", email: "dilukalahiruofficial@gmail.com", role: "Lead" },
+  { name: "Diluka Lahiru", email: "diluka.20234038@iit.ac.lk", role: "Lead" },
+  { name: "Senuja", email: "senuja.20234041@iit.ac.lk", role: "Team Lead" }, 
+  { name: "supuni", email: "supuni.20234044@iit.ac.lk", role: "Developer" },
+  { name: "hirusha", email: "hirusha.20234077@iit.ac.lk", role: "Developer" }, 
+  { name: "avishka", email: "avishka.20234058@iit.ac.lk", role: "Developer" }, 
+  { name: "nirodha", email: "nirodha.20234073@iit.ac.lk", role: "Developer" }, 
+];
+
